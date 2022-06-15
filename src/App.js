64c6fe0eb69d644989bcab40bc8102b08ef0e912
Alejandro <acuellarh@gmail.com>
2022-06-14 19:05:36 -0500

    original de Olga

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+// import { Login } from './components/Login';
+import { Navbar } from './components/Navbar';
+import { Signup } from './components/Signup';
+
+function App() {
+	return (
+		<>			
+			<Navbar/>		
+			<Signup/>
+			{/* <Login/> */}
+		</>
+	);
+}
+
+export default App;
